2f4c4d43809ce1f24f31e91ba64726a508f0b4f6
duvan <duvan@HIEUDU>
2021-09-28 17:35:25 +0700

    Add project files.

---
+++ KendoUiCore/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using KendoUiCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KendoUiCore.Controllers
+{
+    public class CustomerController : Controller
+    {
+       CustomerRepository customerRepository = new CustomerRepository();
+       public JsonResult GetAllCustomer()
+        {
+            var res = customerRepository.GetAllCustomer();
+            var data = new
+            {
+                Items = res,
+                TotalCount = res.Count
+            };
+            return new JsonResult(data);
+        }
+        
+    }
+}
---
+++ KendoUiCore/Models/CustomerRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KendoUiCore.Models
+{
+    public class CustomerRepository
+    {
+        public List<CustomerModel> GetAllCustomer()
+        {
+            var customerList = new List<CustomerModel>()
+            {
+                        new CustomerModel() { CustomerID = "1", ContactName= "Jonh 1", CompanyName= "IVS 1", Country= "US" },
+                        new CustomerModel() { CustomerID= "2", ContactName= "Jonh 2", CompanyName= "IVS 1", Country= "Uk" },
+                        new CustomerModel() { CustomerID= "3", ContactName= "Jonh 3", CompanyName= "IVS 1", Country= "CN" },
+                        new CustomerModel() { CustomerID= "4", ContactName= "Jonh 4", CompanyName= "IVS 1", Country= "VN" },
+                        new CustomerModel() { CustomerID= "5", ContactName= "Jonh 5", CompanyName= "IVS 1", Country= "US" },
+                        new CustomerModel() { CustomerID= "6", ContactName= "Jonh 6", CompanyName= "IVS 1", Country= "HK" },                 
+                        new CustomerModel() { CustomerID= "7", ContactName= "Jonh 7", CompanyName= "IVS 1", Country= "Uk" },
+                        new CustomerModel() { CustomerID= "8", ContactName= "Jonh 8", CompanyName= "IVS 1", Country= "CN" },
+                        new CustomerModel() { CustomerID= "9", ContactName= "Jonh 9", CompanyName= "IVS 1", Country= "VN" },
+                        new CustomerModel() { CustomerID= "10", ContactName= "Jonh 10", CompanyName= "IVS 1", Country= "US" }
+
+            };
+            return customerList;
+        }
+            
+    }
+}
